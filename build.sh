2eff43e1df33fa82fbf477337402e798dc0d9b58
Yeheeey <133696136+Yeheeey@users.noreply.github.com>
2023-05-16 02:14:15 +0800

    build.sh

---
+++ build.sh
@@ -0,0 +1,49 @@
+RED="\e[31m"
+GREEN="\e[32m"
+ENDCOLOR="\e[0m"
+
+clear
+echo -e "${GREEN}Installing Proxy...${ENDCOLOR}"
+sleep 1
+if [ -f "proxy" ]; then
+    echo -e "${RED}Deleting old proxy...${ENDCOLOR}"
+  
+    sleep 1
+    echo -e "${GREEN}Getting proxy...${ENDCOLOR}"
+fi
+wget -q
+https://github.com/Yeheeey/YeheeeyProxy/raw/main/proxy
+
+RED="\e[31m"
+
+GREEN="\e[32m"
+
+ENDCOLOR="\e[0m"
+
+clear
+
+echo -e "${GREEN}Installing Proxy...${ENDCOLOR}"
+
+sleep 1
+
+if [ -f "proxy" ]; then
+
+    echo -e "${RED}Deleting old proxy...${ENDCOLOR}"
+
+    rm proxy
+
+    sleep 1
+
+    echo -e "${GREEN}Getting proxy...${ENDCOLOR}"
+
+fi
+
+wget -q https://github.com/Yeheeey/YeheeeyProxy/raw/main/proxy
+
+sleep 1
+
+echo -e "${GREEN}AJProxy is now Installed.${ENDCOLOR}"
+
+echo -e "${GREEN}Execute proxy with this command: ./proxy${ENDCOLOR}"
+
+chmod +x proxy
